8a20033494b3bceddde48378a0f13e4f4540d31f
Tim Leek <trleek@gmail.com>
2021-05-28 12:18:43 -0400

    changed edge coverage to be in terms of thread, i.e. tid and create time

--- panda/plugins/edge_coverage/edge_coverage.cpp
+++ panda/plugins/edge_coverage/edge_coverage.cpp
@@ -31,12 +31,32 @@ extern "C" {
 
 using namespace std;
 
-typedef target_ulong Asid;
 typedef target_ulong Pc;
 
 target_ulong start_main = 0;
 
-map <Asid, vector<Pc>> asid_trace; 
+
+
+struct Thread {
+    target_ptr_t tid;
+    uint64_t create_time;
+
+    bool operator <(const Thread &other) const {
+        if (this->tid < other.tid) return true;
+        if (this->tid > other.tid) return false;
+        if (this->create_time < other.create_time) return true;
+        return false;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Thread &th) {
+        os << "(Thread," << "tid=" << th.tid << "create_time=" << th.create_time << ")";
+        return os;
+    }
+
+
+};
+
+map <Thread, vector<Pc>> thread_trace; 
 
 bool debug=false;
 
@@ -44,13 +64,34 @@ bool debug=false;
 int n;
 
 
-map<target_ulong,target_ulong> last_bb_start;
-map<target_ulong,bool> last_bb_was_split;
-map<target_ulong,bool> last_bb_was_exception;
-map<target_ulong,bool> last_bb_intexc;
-map<target_ulong,target_ulong> last_bb_before_intexc;
+
+
+map<Thread,target_ulong> last_bb_start;
+map<Thread,bool> last_bb_was_split;
+map<Thread,bool> last_bb_intexc;
+map<Thread,target_ulong> last_bb_before_intexc;
 
 bool saw_main = false;
+
+bool got_current_thread = false;
+Thread current_thread;
+
+// returns true iff current proc and thread are available.
+// also populates *currentp and othreadp
+tuple<bool,OsiProc*,OsiThread*> get_proc_thread(CPUState *cpu) {
+    // need current process and thread and to be able to
+    // log flows at a sink or label a new source
+    OsiProc *current = NULL;
+    OsiThread *othread = NULL;
+    current = get_current_process(cpu); 
+    if (current && current->pid !=0) {
+        othread = get_current_thread(cpu);
+        if (othread) {
+            return make_tuple(true, current, othread);
+        }
+    }
+    return make_tuple(false, current, othread);
+}
 
 
 void after_block(CPUState *env, TranslationBlock *tb, uint8_t exitCode) {
@@ -59,23 +100,27 @@ void after_block(CPUState *env, TranslationBlock *tb, uint8_t exitCode) {
     if (start_main && !saw_main) 
         return;
 
-    target_ulong asid = panda_current_asid(env);
-    
+    if (!got_current_thread) return;
+
     // dont record pc if we are in exception or interrupt code
     if (check_in_exception() || check_in_interrupt()) 
         return;
     
     // dont record pc if last block was split
-    if (last_bb_was_split.count(asid) != 0 && !last_bb_was_split[asid])
-        last_bb_start[asid] = tb->pc;
+    if (last_bb_was_split.count(current_thread) != 0 && !last_bb_was_split[current_thread])
+        last_bb_start[current_thread] = tb->pc;
     
     // keep track of if last bb was split
-    last_bb_was_split[asid] = tb->was_split; 
+    last_bb_was_split[current_thread] = tb->was_split; 
     
 }
 
+// tb size
+map<target_ulong, vector<uint64_t>> tb_size;
 
 void before_block(CPUState *env, TranslationBlock *tb) {
+
+    tb_size[tb->pc].push_back(tb->size);
     
     if (start_main) {
         // we are only paying attention to edges within some program
@@ -87,34 +132,46 @@ void before_block(CPUState *env, TranslationBlock *tb) {
         if (!saw_main)
             return;
     }
+
+    auto [pt_ok, current, othread] = get_proc_thread(env);
+
+    // dont know process/thread
+    if (!pt_ok) {
+        got_current_thread = false;
+        return;
+    }
+
+    got_current_thread = true;
+
+    current_thread.tid = othread->tid;
+    current_thread.create_time = current->create_time;
     
-    target_ulong asid = panda_current_asid(env);
     bool intexc = (check_in_exception() || check_in_interrupt());
     
-    // we can only know transition if we know where we were for this asid last
-    if (last_bb_intexc.count(asid) != 0) {
+    // we can only know transition if we know where we were for this thread last
+    if (last_bb_intexc.count(current_thread) != 0) {
         
         // four possibilities
         
         // 1. transition from reg to intexc code
-        if (!last_bb_intexc[asid] && intexc) {
+        if (!last_bb_intexc[current_thread] && intexc) {
             // remember start pc of last bb before intexc
             if (debug) 
                 cout << "trans from reg to intexc -- saving last_bb_before_intexc[" 
-                     << hex << asid << "]=" << last_bb_start[asid] << "\n";
-            last_bb_before_intexc[asid] = last_bb_start[asid];
+                     << hex << current_thread << "]=" << last_bb_start[current_thread] << "\n";
+            last_bb_before_intexc[current_thread] = last_bb_start[current_thread];
             goto done;
         }
         
         // 2. transition from int/exc code to reg
-        if (last_bb_intexc[asid] && !intexc) {
+        if (last_bb_intexc[current_thread] && !intexc) {
             // if this bb is just same as the last one before
             // the int/exc, we ignore
             if (debug) 
                 cout << "trans from intexc to reg\n";
-            if (tb->pc == last_bb_before_intexc[asid]) {
+            if (tb->pc == last_bb_before_intexc[current_thread]) {
                 cout << "same last bb\n";
-                last_bb_start[asid] = tb->pc;
+                last_bb_start[current_thread] = tb->pc;
                 goto done;
             }
             // bbs have different start pc. 
@@ -123,21 +180,21 @@ void before_block(CPUState *env, TranslationBlock *tb) {
             if (debug) {
                 cout << "not same last bb\n";
                 cout << "adding to trace last_bb_before_intexc["
-                     << hex << asid << "]=" << last_bb_before_intexc[asid] << "\n";
-                cout << "and setting last_bb_start[" << hex << asid << "]=" << tb->pc << "\n";
+                     << hex << current_thread << "]=" << last_bb_before_intexc[current_thread] << "\n";
+                cout << "and setting last_bb_start[" << hex << current_thread << "]=" << tb->pc << "\n";
             }
 
-            asid_trace[asid].push_back(last_bb_before_intexc[asid]);                     
+            thread_trace[current_thread].push_back(last_bb_before_intexc[current_thread]);                     
             // update pc in case we get longjmped
-            last_bb_start[asid] = tb->pc;
+            last_bb_start[current_thread] = tb->pc;
         }
         
         // 3. no transition && we are in regular code
-        if (!last_bb_intexc[asid] && !intexc) {
+        if (!last_bb_intexc[current_thread] && !intexc) {
             // ugh last bb was split so we dont update trace yet
             if (debug) 
                 cout << "no trans and in reg code\n";
-            if (last_bb_was_split[asid]) {
+            if (last_bb_was_split[current_thread]) {
                 if (debug) 
                     cout << "but last bb was split\n";
                 goto done;
@@ -145,14 +202,14 @@ void before_block(CPUState *env, TranslationBlock *tb) {
             if (debug) {
                 cout << "last bb not split\n";
                 cout << "adding to trace last_bb_start["
-                     << hex << asid << "]=" << last_bb_start[asid] << "\n";        
-                cout << "and setting last_bb_start[" << asid << "]=" << tb->pc << "\n";
+                     << hex << current_thread << "]=" << last_bb_start[current_thread] << "\n";        
+                cout << "and setting last_bb_start[" << current_thread << "]=" << tb->pc << "\n";
             }
             
             // update trace in normal way
-            asid_trace[asid].push_back(last_bb_start[asid]);                 
+            thread_trace[current_thread].push_back(last_bb_start[current_thread]);                 
             // update pc in case we get longjmped
-            last_bb_start[asid] = tb->pc; 
+            last_bb_start[current_thread] = tb->pc; 
         }
     }
     
@@ -163,7 +220,7 @@ void before_block(CPUState *env, TranslationBlock *tb) {
     // keep track of last intexc value to be able
     // to observe transition
 done:
-    last_bb_intexc[asid] = intexc;
+    last_bb_intexc[current_thread] = intexc;
 }
 
 
@@ -188,7 +245,7 @@ bool init_plugin(void *self) {
         printf ("edge coverage for just one program: start_main = 0x" TARGET_FMT_lx "\n", start_main);
     }
     else 
-        printf ("edge coverage for all asids and all code\n");
+        printf ("edge coverage for all threads and all code\n");
     
     panda_require("osi");
     assert(init_osi_api()); // Setup OSI inspection
@@ -207,12 +264,14 @@ void uninit_plugin(void *) {
 
     if (!pandalog) return; 
 
-    map<Asid, map<vector<Pc>, int>> final_map; 
+    printf("Writing out pandalog info for edge_coverage\n");
 
-    // From the traces in each asid, collect up to n-edges 
-    for (auto kvp : asid_trace) { 
+    map<Thread, map<vector<Pc>, int>> final_map; 
+
+    // From the traces in each thread, collect up to n-edges 
+    for (auto kvp : thread_trace) { 
         map<vector<Pc>, int> edges;
-        auto asid = kvp.first;
+        auto thread = kvp.first;
         auto pc_trace = kvp.second; 
         ofstream tracef;
         for (int k = 1; k <= n; k++) { 
@@ -223,37 +282,42 @@ void uninit_plugin(void *) {
                     if (i+j < pc_trace.size()) 
                         edge.push_back(pc_trace[i + j]);                     
                 }
-                // Add the edge to the map for this asid and update its count  
+                // Add the edge to the map for this thread and update its count  
                 if (edges.find(edge) != edges.end())  edges[edge] += 1;
                 else  edges[edge] = 1; 
             }
         }
         if (pandalog_trace) {
-            Panda__AsidTrace *at = (Panda__AsidTrace *) malloc (sizeof(Panda__AsidTrace));
-            *at = PANDA__ASID_TRACE__INIT;
-            at->pcs = (uint64_t *) malloc(sizeof(uint64_t) * pc_trace.size());
+            Panda__ThreadTrace *at = (Panda__ThreadTrace *) malloc (sizeof(Panda__ThreadTrace));
+            *at = PANDA__THREAD_TRACE__INIT;
+            at->tid = thread.tid;
+            at->create_time = thread.create_time;
+            at->pcs = (uint64_t *) malloc(sizeof(uint64_t) * pc_trace.size());            
             int i=0;
             for (auto pc : pc_trace) 
                 at->pcs[i++] = pc;
             at->n_pcs = pc_trace.size();	  
             Panda__LogEntry ple = PANDA__LOG_ENTRY__INIT;
-            ple.has_asid = 1;
-            ple.asid = asid;
             ple.trace = at;
             pandalog_write_entry(&ple);
             free(at->pcs);
             free(at);
-            final_map[asid] = edges; 
         }
+        final_map[thread] = edges; 
+        
+        cout << "final_map[" << thread << "] is " << (final_map[thread].size()) << " long\n";
     }
+    
 
     // Write out each to a pandalog 
     for (auto kvp: final_map) { 
-        auto asid = kvp.first;
+        auto thread = kvp.first;
         auto edge_map = kvp.second; 
 
-        Panda__AsidEdges * ae = (Panda__AsidEdges *) malloc (sizeof (Panda__AsidEdges)); 
-        *ae = PANDA__ASID_EDGES__INIT; 
+        Panda__ThreadEdges * ae = (Panda__ThreadEdges *) malloc (sizeof (Panda__ThreadEdges)); 
+        *ae = PANDA__THREAD_EDGES__INIT; 
+        ae->tid = thread.tid;
+        ae->create_time = thread.create_time;
 
         ae->n_edges = edge_map.size(); 
         Panda__Edge ** e = (Panda__Edge **) malloc (sizeof (Panda__Edge *) * edge_map.size()); 
@@ -263,36 +327,59 @@ void uninit_plugin(void *) {
             auto n_edge = kvp.first;
             auto hit_count = kvp.second;
 
+            // some edges we will skip
+            bool skip = false;
+            if (n_edge.size() <= 1) skip = true;
+            if (!skip) {
+                for (auto pc:n_edge) {
+                    if (pc == 0 || tb_size.count(pc) == 0) {
+                        skip = true;
+                        break;
+                    }
+                }
+            }
+            if (skip) 
+                continue;
+            
             e[i] = (Panda__Edge *) malloc (sizeof (Panda__Edge)); 
             *(e[i]) = PANDA__EDGE__INIT;
 
-            //e[i]->n = n_edge.size(); 
-            uint64_t *pc_list = (uint64_t *) malloc (sizeof (uint64_t) * (n_edge.size()));
-            int j = 0;
-            for (auto edge : n_edge) {
-                pc_list[j++] = edge; 
+            Panda__Block **block_list = (Panda__Block **) malloc(sizeof(Panda__Block *) * n_edge.size());
+            
+            int j=0;
+            for (auto pc : n_edge) {                
+                block_list[j] = (Panda__Block *) malloc(sizeof(Panda__Block));
+                *block_list[j] = PANDA__BLOCK__INIT;
+                block_list[j]->pc = pc;
+                if (tb_size[pc][0] == 0) 
+                    block_list[j]->size = 0;
+                else
+                    block_list[j]->size = (uint32_t) tb_size[pc][0];
+                j++;
             }
 
-            e[i]->pc = pc_list;
-            e[i]->n_pc = n_edge.size();
+            e[i]->block = block_list;
+            e[i]->n_block = n_edge.size();
             e[i]->hit_count = hit_count; 
             i++; 
         } 
+        int num_edges = i;
         ae->edges = e; 
+        ae->n_edges = num_edges;
 
 
         Panda__LogEntry ple = PANDA__LOG_ENTRY__INIT; 
-        ple.has_asid = 1;
-        ple.asid = asid; 
         ple.edge_coverage = ae; 
         pandalog_write_entry(&ple); 
 
         // Free everything I used
-        for (int i = 0; i < edge_map.size(); i++) { 
-            free(e[i]->pc);
-            free(e[i]); 
+        for (int i=0; i<num_edges; i++) {
+            for (int j=0; j<e[i]->n_block; j++) {
+                free(e[i]->block[j]);
+            }
+            free(e[i]->block);
         }
-        free(e); 
+        free(e);
         free(ae); 
     } 
 
